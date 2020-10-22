3550ae7f0d076f14cf671dfdd42d28c1978f8de9
Sam Hamburger <samhamburger@MacBook-Pro-2.lan1>
2020-10-22 08:53:13 -0700

    favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,7 @@ export default {
       { hid: 'description', name: 'description', content: '' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/port-logo.png' },
       {
         rel: 'stylesheet',
         href:
